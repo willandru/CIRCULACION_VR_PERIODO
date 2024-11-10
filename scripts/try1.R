8eb04f4ffe8e22427cc3bfc88c6e47a7d01aebb5
Willandru <willandru@hotmail.com>
2024-11-10 10:11:02 -0500

    intentando mejorar la ilustracion de ggplot

--- scripts/try1.R
+++ scripts/try1.R
@@ -51,6 +51,7 @@ line_data <- data %>%
 # AJUSTAMOS LA MAGNITUD DESEADA DE LOS EJES 'Y' DE LA GRÁFICA
 EJEY_NUM_CASOS_VALOR_MAX <- 700
 EJEY_PORC_POSI_VALOR_MAX <- 70
+scaling_factor <- EJEY_NUM_CASOS_VALOR_MAX / EJEY_PORC_POSI_VALOR_MAX
 # Calculate the scaling factor for dual y-axes
 scaling_factor <- 700 / 70 # Scale Cases to match a max of 700 on the left and 70% on the right
 
@@ -58,9 +59,25 @@ scaling_factor <- 700 / 70 # Scale Cases to match a max of 700 on the left and 7
 #Necesitamos crear los posibles tamaños (1 tamaño para este dataset pero pueden haber mas pequeños).
 #parece que se ajusta esto directamente en RMarkDown
 
-ANCHO_DE_LAS_BARRAS <- 0.4
-ANCHO_DE_LA_LINEA <- 0.7
+ANCHO_BARRAS <- 0.4
+COLOR_AH1N1pdm09 <- "#8064A2"
+COLOR_A_no_subtipificado <- "#4BACC6"
+COLOR_AH3 <- "#F79646"
+COLOR_Influenza__B <- "#2C4D75"
+COLOR_Adenovirus <- "#4D3B62"
+COLOR_Metapneumovirus <- "#2C4D75"
+COLOR_Rinovirus <- "#B65708"
+COLOR_Bocavirus <- "#729ACA"
+COLOR_Otros_Virus <- "#4F81BD"
+COLOR_Parainfluenza <- "#772C2A"
+COLOR_VSR <- "#5F7530"
+COLOR_nueva_columna <- "black"
 
+
+
+
+ANCHO_LINEA <- 0.7
+COLOR_LINEA <- "#E97132"
 
 
 ggplot() +
@@ -68,7 +85,7 @@ ggplot() +
   geom_bar(data = stacked_data, 
            aes(x = YearWeek, y = Cases, fill = Virus_Type), 
            stat = "identity",
-           width = 0.4) +
+           width = ANCHO_BARRAS) +
   
   # Line chart for % positivity with scaling applied
   geom_line(data = line_data, 
@@ -76,35 +93,48 @@ ggplot() +
                 y = Percent_Positivity * scaling_factor, 
                 color = "Positivity Rate", 
                 group = 1),
-            color = "#E97132",
-            linewidth = 0.7) +
+            color = COLOR_LINEA,
+            linewidth = ANCHO_LINEA) +
   
-  # Scale and labels with specified y-axis breaks
+  # EJE Y: Escala y nombres de los ejes. Ubicacion de la grilla de fondo
   scale_y_continuous(name = "NÚMERO DE CASOS POSITIVOS",
                      limits = c(-500, 700), breaks = seq(0, 700, by = 100),
                      sec.axis = sec_axis(~ . / scaling_factor, 
                                          breaks = seq(0, 70, by = 10), 
                                          labels = function(x) sprintf("%.1f", x))
   ) +
+  
+  
   scale_x_discrete(labels = tabla$PERIODO_EPIDEMIOLOGICO)+
+  
+  
   scale_fill_manual(values = c(
-    "A(H1N1)pdm09" = "#8064A2",       # Light blue for A(H1N1)pdm09
-    "A_no_subtipificado" = "#4BACC6", # Purple for A no subtipificado
-    "A(H3)" = "#F79646",              # Green for A(H3)
-    "Influenza__B" = "#2C4D75",       # Dark gray for Influenza B
-    "Adenovirus" = "#4D3B62",         # Dark teal for Adenovirus
-    "Metapneumovirus" = "#2C4D75",    # Dark purple for Metapneumovirus
-    "Rinovirus" = "#B65708",          # Dark green for Rinovirus
-    "Bocavirus" = "#729ACA",          # Blue for Bocavirus
-    "Otros_Virus" = "#4F81BD",        # Blue for Otros Virus
-    "Parainfluenza" = "#772C2A",      # Brown for Parainfluenza
-    "VSR" = "#5F7530",                # Dark green for VSR
-    "nueva_columna" = "black"         # Black for nueva_columna (appears as line in legend)
+    "A(H1N1)pdm09" = COLOR_AH1N1pdm09,
+    "A_no_subtipificado" = COLOR_A_no_subtipificado,
+    "A(H3)" = COLOR_AH3,
+    "Influenza__B" = COLOR_Influenza__B,
+    "Adenovirus" = COLOR_Adenovirus,
+    "Metapneumovirus" = COLOR_Metapneumovirus,
+    "Rinovirus" = COLOR_Rinovirus,
+    "Bocavirus" = COLOR_Bocavirus,
+    "Otros_Virus" = COLOR_Otros_Virus,
+    "Parainfluenza" = COLOR_Parainfluenza,
+    "VSR" = COLOR_VSR,
+    "nueva_columna" = COLOR_nueva_columna
   ))+
+  
+  
   labs(x = "PERÍODO EPIDEMIOLÓGICO", fill = NULL, color = NULL) +
+  
+  
+  
+  
+  
   
   # Customize the grid lines
   theme_minimal() +
+  
+  
   theme(
     axis.text.x = element_text(angle = 0,size=7,  margin = margin(t = -255, b=-5)),
     axis.title.x = element_text(margin = margin(t = 20, b=-10), size = 8, face = "bold", color = "#595959"),
@@ -117,16 +147,26 @@ ggplot() +
     legend.key.height = unit(0.02, "lines"),
     legend.text = element_text(size = 7)
   )+
+  
+  
   # Agregar líneas verticales con altura ajustable usando geom_segment
   geom_segment(aes(x = 13.5, xend = 13.5, y = -25, yend = 700), color = "black", linewidth = 0.65) +
   geom_segment(aes(x = 26.5, xend = 26.5, y = -25, yend = 700), color = "black", linewidth = 0.65) +
+  
+  
+  
   annotate("text", x = c(6, 19, 31), y = -35, label = c("AÑO 2022", "AÑO 2023", "AÑO 2024"), size = 2.4, fontface = "bold") +
+  
+  
   guides(
     fill = guide_legend(
       nrow = 3,
       byrow = TRUE
     ),
     color = "none") + 
+  
+  
+  
   annotate("segment", x = 21, xend = 22.2, y = -150, yend = -150, color = "#E97132", linewidth = 0.7) +
   annotate("text", x = 22.5, y = -150, label = "% DE POSITIVIDAD", hjust = 0, color = "black", size= 2) 
 
